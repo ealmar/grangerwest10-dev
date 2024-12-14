92ca7f7deb6f731629f59d53bea96159c38f9e00
ealmar <112268412+ealmar@users.noreply.github.com>
2024-12-13 21:13:17 -0700

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+var lastChange = document.lastModified;
+document.getElementById("last-updated").innerHTML =  "Ultima actualizacion: " + lastChange;
+
+
+document.querySelector(".banner_close").addEventListener("click", function(){
+alert(1)
+});
